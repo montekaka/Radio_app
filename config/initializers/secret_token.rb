3d30a91ea5176f629f3531587bc608f5e4d51488
montekaka <josh.chen.jia@gmail.com>
2012-12-13 22:51:29 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RadioApp::Application.config.secret_token = '865a3d2164abc5dbbb450ee3d6ecf52d1258cb493ccf5cac6ba9342fbd79ed93b08d4aea383fea48df7765b8035c96ed1d961e08b4edb0f787183663d0e186ca'
