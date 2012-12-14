cb2689408b496494e0ce8dab9938b487a36f951f
montekaka <josh.chen.jia@gmail.com>
2012-12-13 23:12:54 -0800

    setup the user and audiopost relationship

--- app/models/audiopost.rb
+++ app/models/audiopost.rb
@@ -1,2 +1,8 @@
 class Audiopost < ActiveRecord::Base
+	attr_accessible :title, :short_note, :user_id, :audio, :audio_date
+	
+	belongs_to :user
+
+	validates :short_note, :length=>{ :maximum => 140 }
+	validates :title, :length=>{ :maximum => 50 }
 end
