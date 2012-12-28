cea82397a53fab8b9815da73454bff632a4276e7
Roland <chan0123@hotmail.com>
2012-12-28 14:03:45 -0800

    modify the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+
+Audiopost.destroy_all
+Audiopost.create(:title => "俄敍通婚/通緝名單", :audio => "http://www.am1300.com/uploadfile/2012/0704/20120704022610296.mp3", :audio_date => "07/03/2012")
