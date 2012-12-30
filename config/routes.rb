63a7c30df4c7d5d7bc3c07952b6e7a3ee2bb4260
Roland <chan0123@hotmail.com>
2012-12-30 10:45:46 -0800

    change the home page to audioposts and rename index.html

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ RadioApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'audioposts#index'
 
   # See how all your routes lay out with "rake routes"
 
