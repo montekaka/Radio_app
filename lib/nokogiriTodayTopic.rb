69c1f287edefeaf988591d7f986b51d433e089c3
montekaka <josh.chen.jia@gmail.com>
2013-04-24 21:56:30 -0700

    remove the first row because its nil

--- lib/nokogiriTodayTopic.rb
+++ lib/nokogiriTodayTopic.rb
@@ -10,21 +10,23 @@ def getShowPlayListTable(showURL)
 
 	page = Nokogiri::HTML(open(showURL))
 	table = page.css('table#type_1 tr')
+	#remove the first row because its nil
+	table= table[1..table.length]
 
 	dummyEpisodeTitle = []
 	dummyEpisodeURL = []
 	dummyEpisodeDate = []
 
-	 table.each do |t|
+	table.each do |t|
 	 	dummyURL = t.css('td:nth-child(1) a')
 		dummyURL.each do |u|
 			dummyEpisodeURL.push(u['href'].strip)
-			dummyEpisodeTitle.push(u.text.strip)			
+			dummyEpisodeTitle.push(u.text.strip)
 		end
-		dummyDate = t.css('td:nth-child(3)').text.strip		
+		dummyDate = t.css('td:nth-child(3)').text.strip
+
 		dummyDate.each do |d|			
-			#puts Date.strptime(ddate, '%m/%d/%Y')
-			puts d
+			#puts Date.strptime(ddate, '%m/%d/%Y')			
 			if d != "週一~週五"
 				ddate=d[0..2]+d[3..5]+'20'+d[6..7]
 				episodeDate = ddate
@@ -47,12 +49,12 @@ def getShowPlayListTable(showURL)
 end
 
 
-# fy = getShowPlayListTable('http://www.am1300.com/index.php?m=content&c=index&a=lists&catid=37&page=6')
-# fy.each do |f|
-# 	puts f.name
-# 	puts f.date
-# 	puts f.url
-# end
+fy = getShowPlayListTable('http://www.am1300.com/index.php?m=content&c=index&a=lists&catid=37')
+fy.each do |f|
+	puts f.name
+	puts f.date
+	puts f.url
+end
 
 def updateAudiopost(showHTML, firstTime, dbLastDay)
     audiopostUpdateArray = []
