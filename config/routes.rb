428b8cc7e06dfc00543978e57c08291456b7a240
Roland <chan0123@hotmail.com>
2013-05-05 10:57:34 -0700

    change the index page to "stations#index" change the station#index link to categories page change the back button of show#index page back to categories page

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ RadioApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'shows#index'
+  root :to => 'stations#index'
 
   # See how all your routes lay out with "rake routes"
 
