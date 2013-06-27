f68680456398f4bd58844a2d642189b3d824e941
montekaka <josh.chen.jia@gmail.com>
2013-06-26 23:32:46 -0700

    adding money radio nokogiri

--- lib/nokogiriMoneyRadio.rb
+++ lib/nokogiriMoneyRadio.rb
@@ -22,3 +22,26 @@ class EpisodePage
 	attr_accessor :name, :desc, :date, :url, :id, :showID
 end
 
+
+def getEpisodeInfo(showURL)
+	page = Nokogiri::HTML(open(showURL))
+	showTitle = page.css('td#contentTd table td h3 strong.title').text.strip
+	showDescription = page.css('td#contentTd p').text.strip
+	episodeTable = page.css('td#contentTd table td a.hmenu')
+	episodeTitle = []
+	episodeURL = []
+
+	episodeTable.each do |t|
+		#episodeURL.push('http://www.moneyradio.org/'+t['href']) 
+		episodeTitle.push(t.text.strip)
+	end
+
+	episodeLinks = page.css('td#contentTd table td a[target="_blank"]')
+	episodeLinks.each do |i|
+		episodeURL.push('http://www.moneyradio.org/'+i['href'])
+	end
+	return showDescription
+end
+
+puts getEpisodeInfo("http://www.moneyradio.org/showSubCategory.php?SCID=279")
+
