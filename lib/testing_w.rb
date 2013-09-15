bcf0555db9db5771e249c06aad77e76cc966cc6e
montekaka <josh.chen.jia@gmail.com>
2013-09-14 17:13:07 -0700

    Adding the parse file title function

--- lib/testing_w.rb
+++ lib/testing_w.rb
@@ -1,3 +1,29 @@
+require 'date'
+def parse_podcast_name(path)	
+	while path.index('/') != nil do
+		i = path.index('/')+1
+		path = path[i..path.length]
+	end
+
+	rev_path = path.reverse
+	if rev_path.index('.').nil?
+		i = 0
+	else
+		i = rev_path.index('.')+1
+	end
+	new_path = rev_path[i..rev_path.length]
+
+	file_Name = new_path.reverse
+	possible_Date = new_path[0..9].reverse
+
+	# if Date.strptime(possible_Date, '%Y-%m-%d')
+	# 	file_Date = nil
+	# else
+	# 	file_Date = Date.strptime(possible_Date, '%Y-%m-%d')
+	# end
+	return Date._parse('abc')[:year]
+end
+
 require 'dropbox_sdk'
 
 # APP_KEY = 'xs74qq784d9t162'
@@ -16,7 +42,9 @@ client = DropboxClient.new(access_token)
 
 prev_delta = client.delta()
 prev_cursor = prev_delta['cursor']
+#puts prev_cursor
 #puts prev_cursor.to_s
 prev_delta['entries'].each do |e|
-	puts e[1]['is_dir']
+	#puts e[1]['path']	
+	puts parse_podcast_name(e[1]['path'])
 end
